93888daee267ce2e7a18b52fc2c731773887bf3b
Will <wivorn.c@gmail.com>
2019-03-09 00:33:54 -0500

    Add home page and recipe card

---
+++ src/components/RecipeCard.vue
@@ -0,0 +1,117 @@
+<template>
+  <a class="recipe-card" :href="url" :data-lang="lang">
+    <div class="shine"></div>
+    <div class="image" :style="{ backgroundImage: `url('` + require(`@/assets/${image}`) + `')` }"></div>
+    <h1>{{ name }}</h1>
+  </a>
+</template>
+
+<script>
+export default {
+  name: "RecipeCard",
+  props: {
+    image: String,
+    name: String,
+    url: String,
+    lang: String
+  },
+  data() {
+    return {
+      left: 0,
+      top: 0,
+      offsetFactor: 6
+    };
+  },
+  methods: {
+    mouseMoveHandler: function(e) {
+      const offsetX = 0.5 - (e.pageX - this.left) / this.$el.clientWidth;
+      const offsetY = 0.5 - (e.pageY - this.top) / this.$el.clientHeight;
+      const dx = e.pageX - this.left - this.$el.clientWidth / 2;
+      const dy = e.pageY - this.top - this.$el.clientHeight / 2;
+      let angle = (Math.atan2(dy, dx) * 180) / Math.PI;
+
+      if (angle < 0) {
+        angle += 360;
+      }
+
+      this.$el.setAttribute(
+        "style",
+        `transform: perspective(800px) translateY(${-offsetY *
+          this.offsetFactor}px) rotateX(${-offsetY *
+          this.offsetFactor}deg) rotateY(${offsetX * this.offsetFactor}deg)`
+      );
+
+      this.$el.firstElementChild.setAttribute(
+        "style",
+        `background: linear-gradient(${angle -
+          90}deg, rgba(255,255,255,${(e.pageY - this.top) /
+          this.$el.clientHeight /
+          4}) 0%, rgba(255,255,255,0) 100%)`
+      );
+    },
+    mouseLeaveHandler: function(e) {
+      this.$el.setAttribute(
+        "style",
+        `transform: perspective(800px) translateY(0) rotateX(0) rotateY(0)`
+      );
+      this.$el.firstElementChild.setAttribute(
+        "style",
+        "background: transparent"
+      );
+    }
+  },
+  mounted() {
+    this.$el.addEventListener("mousemove", this.mouseMoveHandler);
+    this.$el.addEventListener("mouseleave", this.mouseLeaveHandler);
+    this.left = this.$el.getBoundingClientRect().left;
+    this.top = this.$el.getBoundingClientRect().top;
+  },
+  beforeDestroy() {
+    this.$el.removeEventListener("mousemove", this.mouseMoveHandler);
+    this.$el.removeEventListener("mouseleave", this.mouseLeaveHandler);
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.recipe-card {
+  position: relative;
+  overflow: hidden;
+  flex: 1 0 300px;
+  max-width: 300px;
+  margin: 20px;
+  border-radius: 6px;
+  background: #f9f9f9;
+  box-shadow: 0 30px 60px rgba(0, 0, 0, 0.2);
+  transform-style: preserve-3d;
+  transition: transform 100ms;
+  cursor: pointer;
+  text-decoration: none;
+  color: #2c3e50;
+
+  .shine {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: transparent;
+    z-index: 100;
+  }
+
+  .image {
+    width: 100%;
+    height: 160px;
+    background-size: cover;
+    background-position: center;
+  }
+
+  h1 {
+    font-size: 20px;
+  }
+
+  &[data-lang="th"] {
+    font-family: "Kanit";
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>HomeCook</h1>
+    <div class="recipes">
+      <RecipeCard
+        v-for="recipe in recipes"
+        :key="recipe.name"
+        :image="recipe.image"
+        :name="recipe.name"
+        :url="recipe.url"
+        :lang="recipe.lang"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import RecipeCard from "@/components/RecipeCard.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld
+    RecipeCard
+  },
+  data() {
+    return {
+      recipes: [
+        {
+          name: "Beef Udon",
+          image: "beef_udon.jpg",
+          url: "https://www.justonecookbook.com/beef-udon/"
+        },
+        {
+          name: "Zaru Soba",
+          image: "cold_soba.jpg",
+          url: "https://www.justonecookbook.com/zaru-soba-cold-soba-noodles/"
+        },
+        {
+          name: "Chawanmushi",
+          image: "steam_egg.jpg",
+          url:
+            "https://www.justonecookbook.com/chawanmushi-savory-steamed-egg-custard/"
+        },
+        {
+          name: "กุ้งอบวุ้นเส้น",
+          image: "shrimp_with_vermicelli.jpg",
+          url: "https://food.mthai.com/food-recipe/125476.html",
+          lang: "th"
+        }
+      ]
+    };
   }
-}
+};
 </script>
+
+<style>
+.recipes {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  max-width: 1080px;
+  margin: 0 auto;
+}
+</style>
+
