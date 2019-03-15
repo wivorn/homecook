8eb508b49ec5f46af3a440df9d4227a940ad2e5d
Will <wivorn.c@gmail.com>
2019-03-14 21:24:05 -0400

    Fix scroll navigation issue

--- src/router.js
+++ src/router.js
@@ -17,4 +17,11 @@ export default new Router({
       component: () => import('./views/RecipeDetail.vue'),
     },
   ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  },
 })
